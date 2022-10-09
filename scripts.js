fc3f33f26fa4f0e527e0586162ec4a08c8ee3e1e
ilyakochev <marsianaries@gmail.com>
2022-10-09 19:14:55 +0500

    start

---
+++ scripts.js
@@ -0,0 +1,45 @@
+// ------------ Main Menu -------------------
+
+var menu_trigger = true;
+var menu = document.getElementById('main-menu');
+var document_width = document.body.clientWidth;
+var main_menu_button = document.getElementById('main-menu-button');
+
+window.addEventListener("load", function(event) {
+    widthCheck();
+  });
+
+window.addEventListener("resize", function(event) {
+    window.setTimeout(function() {
+      document_width = document.body.clientWidth;
+      widthCheck();
+    }, 500);
+}, true);
+
+function menuTrigger(){
+    menu_trigger = (menu_trigger == true) ? false : true;
+    menuDisplay();  
+}
+
+function menuDisplay(){
+  if( menu_trigger == true ){
+      menu.style.display = 'flex';
+    } else {
+      menu.style.display = 'none';
+    }
+}
+
+function widthCheck(){
+  if( document_width >= 1050 ){
+      menu_trigger = true;
+      menuDisplay();
+    } else {
+      menu_trigger = false;
+      menuDisplay();
+    }
+    return menu_trigger;
+}
+
+main_menu_button.onclick = function(){
+  menuTrigger();
+};
